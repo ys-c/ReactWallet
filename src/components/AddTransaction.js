6a55331a25252bf60d37cad3d399c8a0f803ba67
ys-c <ys.chiaw@gmail.com>
2024-04-06 23:24:31 +0800

    first commit
    Attempting to include an errror checker for the field amount in addTransaction page. attempt is a for a helpertext to appear when a value less than 0 appears even though the min is set to 0
    porblems experienced is that the field becomes uneditable after user attempts to re enter a proper value after an error value
    
    helper text is same color as background

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,65 +1,80 @@
 import TopBar from "./common/TopBar";
-import {useState} from 'react';
-import {useNavigate } from "react-router-dom"
-import {Input,Select,Option,FormControl,FormLabel, Button, MenuItem } from '@mui/joy';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom"
+import { Input, Select, Option, FormControl, FormLabel, Button, FormHelperText } from '@mui/joy';
 import { createTransactionItem } from "../api";
 const AddTransaction = () => {
-    const [transactionDetails, setTransactionDetails] = useState([{amount:'', category:'',date:'',notes:'' }]);
+    const [transactionDetails, setTransactionDetails] = useState([{ amount: '', category: '', date: '', notes: '' }]);
     const navigate = useNavigate();
-    const handleSubmit = (e) =>{
+    const [validAmount, setValidAmount] = useState('true');
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("submit pressed"); 
-        if(transactionDetails[0].amount === "" || transactionDetails[0].category === "" || transactionDetails[0].date === "")
-        {
+        console.log("submit pressed");
+        if (transactionDetails[0].amount === "" || transactionDetails[0].category === "" || transactionDetails[0].date === "") {
             console.log("required not fill up");
         }
         console.log(transactionDetails);
-        
+
     }
-    
-      const handleFormChange = (e) =>{
+
+    const handleFormChange = (e) => {
         let data = transactionDetails;
-        console.log(e.target.name );
+        console.log(e.target.name);
         console.log(e.target.value);
         data[e.target.name] = e.target.value;
         setTransactionDetails(data);
-        
+
+    }
+    const handleAmountChange = (e) => {
+        if (e.target.value <0) {
+            console.log("throw error");
+            setValidAmount('false');
+        }
+        else {
+            setValidAmount('true');
+        }
+        let data = transactionDetails;
+        data[e.target.name] = e.target.value;
+        setTransactionDetails(data);
+
     }
 
-    const handleSelectChange =(e,newValue) =>{
+    const handleSelectChange = (e, newValue) => {
         console.log("newValue", newValue);
         let data = transactionDetails;
         data["category"] = newValue;
         setTransactionDetails(data);
+    };
 
-};
-    
-    const goBack= ()=>{
-       navigate("/home");
+    const goBack = () => {
+        navigate("/home");
     }
     return (
         <div>
             <TopBar banner="Add Transaction" />
             <div className="add-transcation-container">
                 <FormControl >
-                    <FormLabel sx={{mt: 1}}>Amount: </FormLabel>
+                    <FormLabel sx={{ mt: 1 }}>Amount: </FormLabel>
                     <Input size="lg"
                         type="number"
                         slotProps={{
                             input: {
-                              min: 0
-                            },}}
+                                min: 0
+                            },
+                        }}
                         name="amount"
                         placeholder="Amount"
                         startDecorator={'SGD$'}
                         variant="soft"
                         value={transactionDetails.amount}
-                        onChange={(e) => handleFormChange(e)}
-                        required>
-                    </Input>
-                    </FormControl >
-                    <FormControl >
-                    <FormLabel sx={{mt: 2}}>Category: </FormLabel>
+                        onChange={(e) => handleAmountChange(e)}
+                        required />
+                    {validAmount === 'true' ? '' : <FormHelperText> Oops! Please enter valid positive number. </FormHelperText>}
+
+
+                </FormControl >
+                <FormControl >
+                    <FormLabel sx={{ mt: 2 }}>Category: </FormLabel>
                     <Select
                         size="lg"
                         placeholder="Select Category..."
@@ -73,9 +88,9 @@ const AddTransaction = () => {
                         <Option value="Personal Care" onClick={(e) => handleSelectChange(e, "Personal Care")}> Personal Care</Option>
                         <Option value="Income" onClick={(e) => handleSelectChange(e, "Income")}> Income</Option>
                     </Select>
-                    </FormControl >
-                    <FormControl>
-                    <FormLabel sx={{mt: 2}}>Transaction Date: </FormLabel>
+                </FormControl >
+                <FormControl>
+                    <FormLabel sx={{ mt: 2 }}>Transaction Date: </FormLabel>
                     <Input
                         size="lg"
                         type="date"
@@ -85,9 +100,9 @@ const AddTransaction = () => {
                         onChange={(e) => handleFormChange(e)}
                         required>
                     </Input>
-                    </FormControl>
-                    <FormControl>
-                    <FormLabel sx={{mt: 2}}>Transaction Notes: </FormLabel>
+                </FormControl>
+                <FormControl>
+                    <FormLabel sx={{ mt: 2 }}>Transaction Notes: </FormLabel>
                     <Input
                         size="lg"
                         type="text"
@@ -98,8 +113,8 @@ const AddTransaction = () => {
                         variant="soft">
                     </Input>
                 </FormControl>
-                <Button size="lg" variant="solid" sx={{m: 2}} onClick={(e)=>handleSubmit(e)}>Save</Button>
-                <Button size="lg" variant="solid" sx={{m: 2}} onClick={()=>goBack()}>Cancel</Button>
+                <Button size="lg" variant="solid" sx={{ m: 2 }} onClick={(e) => handleSubmit(e)}>Save</Button>
+                <Button size="lg" variant="solid" sx={{ m: 2 }} onClick={() => goBack()}>Cancel</Button>
 
 
             </div>
